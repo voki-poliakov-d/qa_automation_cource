a47e057fa3e0b3cf00d5c382992b5b61574325da
Denis Poliakov <denis.poliakov@vokigames.com>
2025-04-01 23:55:53 +0300

    Попытка 2

---
+++ Calculator.py
@@ -0,0 +1,52 @@
+def add (a, b= None):
+    if b is None:
+        if isinstance(a, (list, tuple, set)):
+            return sum(a)
+        return a
+    return a + b
+
+def subtract (a, b):
+    return a - b
+
+def multiply (a, b):
+    return a * b
+
+def divide (a, b):
+    return a / b
+
+import re
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+}
+
+def calculate():
+    pattern = r'^\s*(-?\d+(\.\d+)?)\s*([\+\-\*/])\s*(-?\d+(\.\d+)?)\s*$'
+
+    while True:
+        expr = input("Введите пример: ")
+
+        match = re.match(pattern, expr)
+        if match:
+            num1, _, operator, num2, _ = match.groups()
+            num1 = float(num1)
+            num2 = float(num2)
+
+            result = operations[operator](num1, num2)
+            print(f"Результат: {result}")
+            break
+
+        else:
+            print("Ошибка: некорректный ввод. Попробуйте снова.")
+
+
+
+calculate()
+
+
+
+
+
